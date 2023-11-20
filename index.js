375a8f9d4663c1a5b8703d3f06fbdf8c6ab93c9a
Carmen Li <cmnli95@gmail.com>
2023-11-19 22:21:22 -0500

    carmen li pushing phase 1 array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+// find matching
+function findMatching(drivers,name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+//fuzzy match
+function fuzzyMatch(drivers,letters){
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+//match name
+function matchName(drivers,name){
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
